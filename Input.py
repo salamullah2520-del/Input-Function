713e66179039602f50274e4b7717685cb49b311b
salamullah2520-del <salamullah2520@gmail.com>
2026-02-18 00:39:28 +0300

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,9 @@
+name = input("What is your name: ")
+age = int(input("What is your age: "))
+balance = float(input("What is your account balance: "))
+
+print("Here is the data you entered: ")
+
+print("Name: ", name)
+print("Age: ", age)
+print("Balance: ", balance)
